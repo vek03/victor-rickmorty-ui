b3a99ab48e27dd89aa349bd2f3d980bd05e35e38
vek03 <vektromboni@gmail.com>
2025-11-13 12:23:19 -0300

    refactor: change marvel theme to rickmorty theme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# VictorMarvelUi
+# VictorRickMortyUi
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.2.15.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'victor-marvel-ui';
+  title = 'victor-rickmorty-ui';
 }
