b6775f698d5e9f30b9fa15f69f1054f653b41314
vek03 <vektromboni@gmail.com>
2025-11-14 13:33:04 -0300

    fix: app.component test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('victor-rickmorty-ui');
   });
 
-  it('should render title', () => {
+  it('should render router-outlet', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, victor-rickmorty-ui');
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
   });
 });
